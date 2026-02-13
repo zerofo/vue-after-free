c26d81a07af1989292223906d19c36c0c5d11dab
zerofo <zerofolh@gmail.com>
2026-02-13 21:01:15 +0800

    更换meme背景..   关闭音乐. 修改默认配置z

--- src/download0/config.ts
+++ src/download0/config.ts
@@ -1,11 +1,14 @@
 export const CONFIG = {
   autolapse: false,
-  autopoop: false,
-  autoclose: false,
-  music: true,
+  autopoop: true,
+  autoclose: true,
+  music: false,
   jb_behavior: 0
 }
 
 export const payloads = [ // to be ran after jailbroken
 //  '/mnt/sandbox/download/CUSA00960/payloads/example_payload.elf'
+ '/mnt/sandbox/download/CUSA00960/payloads/goldhen.bin',
+ '/mnt/sandbox/download/CUSA00960/payloads/fake-signin.bin'
+
 ]
