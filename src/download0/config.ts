73f0bfdbd5d9ec2f1383116db3f0e6dfaa8e0a97
zerofo <46804648+zerofo@users.noreply.github.com>
2026-02-13 13:01:59 +0000

    chore: eslint auto-fix

--- src/download0/config.ts
+++ src/download0/config.ts
@@ -8,7 +8,7 @@ export const CONFIG = {
 
 export const payloads = [ // to be ran after jailbroken
 //  '/mnt/sandbox/download/CUSA00960/payloads/example_payload.elf'
- '/mnt/sandbox/download/CUSA00960/payloads/goldhen.bin',
- '/mnt/sandbox/download/CUSA00960/payloads/fake-signin.bin'
+  '/mnt/sandbox/download/CUSA00960/payloads/goldhen.bin',
+  '/mnt/sandbox/download/CUSA00960/payloads/fake-signin.bin'
 
 ]
